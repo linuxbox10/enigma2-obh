a9afd7a4934960b37bd9702d8be2fa9c01637b87
linuxbox10 <jaysmith940@hotmail.co.uk>
2020-09-11 09:47:48 +0100

    Update skin.py

--- skin.py
+++ skin.py
@@ -11,7 +11,7 @@ from Tools.Directories import SCOPE_CONFIG, SCOPE_CURRENT_LCDSKIN, SCOPE_CURRENT
 from Tools.Import import my_import
 from Tools.LoadPixmap import LoadPixmap
 
-DEFAULT_SKIN = "slyk-q-1080/skin.xml"
+DEFAULT_SKIN = "slyk-onyx-1080/skin.xml"
 EMERGENCY_SKIN = "skin_default/skin.xml"
 EMERGENCY_NAME = "Default OE-A"
 DEFAULT_DISPLAY_SKIN = "skin_default/skin_display.xml"
